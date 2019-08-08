f48aa33ec664494e441ed6c3638ba9cb943dfdda
Whu-wxy <641645560@qq.com>
2019-08-08 17:06:54 +0800

    mypc

--- main.py
+++ main.py
@@ -2,47 +2,18 @@
 import numpy as np
 import cv2
 
-# loading images
-image1 = cv2.imread("journal1.jpg")
-image2 = cv2.imread("news1.jpg")
-image3 = cv2.imread("CookingAtItsBest.png")
+from matplotlib import pyplot as plt
+import numpy as np
 
-# hardcoded assigning of output images for the 3 input images
-output1_letter = image1.copy()
-output1_word = image1.copy()
-output1_line = image1.copy()
-output1_par = image1.copy()
-output1_margin = image1.copy()
-
-output2_letter = image2.copy()
-output2_word = image2.copy()
-output2_line = image2.copy()
-output2_par = image2.copy()
-output2_margin = image2.copy()
-
-
-output3_letter = image3.copy()
-output3_word = image3.copy()
-output3_line = image3.copy()
-output3_par = image3.copy()
-output3_margin = image3.copy()
-
-gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-gray3	= cv2.cvtColor(image3, cv2.COLOR_BGR2GRAY)
-
-# clean the image using otsu method with the inversed binarized image
-ret1,th1 = cv2.threshold(gray1,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-ret2,th2 = cv2.threshold(gray2,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-ret3,th3 = cv2.threshold(gray3,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+import timeit
 
 #processing letter by letter boxing
-def process_letter(thresh,output):	
+def process_letter(binary_img,output):	
 	# assign the kernel size	
 	kernel = np.ones((2,1), np.uint8) # vertical
 	# use closing morph operation then erode to narrow the image	
-	temp_img = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel,iterations=3)
-	# temp_img = cv2.erode(thresh,kernel,iterations=2)		
+	temp_img = cv2.morphologyEx(binary_img,cv2.MORPH_CLOSE,kernel,iterations=3)
+	# temp_img = cv2.erode(binary_img,kernel,iterations=2)		
 	letter_img = cv2.erode(temp_img,kernel,iterations=1)
 	
 	# find contours 
@@ -57,13 +28,13 @@ def process_letter(thresh,output):
 
 
 #processing letter by letter boxing
-def process_word(thresh,output):	
+def process_word(binary_img,output):	
 	# assign 2 rectangle kernel size 1 vertical and the other will be horizontal	
 	kernel = np.ones((2,1), np.uint8)
 	kernel2 = np.ones((1,4), np.uint8)
 	# use closing morph operation but fewer iterations than the letter then erode to narrow the image	
-	temp_img = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel,iterations=2)
-	#temp_img = cv2.erode(thresh,kernel,iterations=2)	
+	temp_img = cv2.morphologyEx(binary_img,cv2.MORPH_CLOSE,kernel,iterations=2)
+	#temp_img = cv2.erode(binary_img,kernel,iterations=2)	
 	word_img = cv2.dilate(temp_img,kernel2,iterations=1)
 	
 	(contours, _) = cv2.findContours(word_img.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -75,13 +46,13 @@ def process_word(thresh,output):
 	return output	
 
 #processing line by line boxing
-def process_line(thresh,output):	
+def process_line(binary_img,output):	
 	# assign a rectangle kernel size	1 vertical and the other will be horizontal
 	kernel = np.ones((1,5), np.uint8)
 	kernel2 = np.ones((2,4), np.uint8)	
 	# use closing morph operation but fewer iterations than the letter then erode to narrow the image	
-	temp_img = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel2,iterations=2)
-	#temp_img = cv2.erode(thresh,kernel,iterations=2)	
+	temp_img = cv2.morphologyEx(binary_img,cv2.MORPH_CLOSE,kernel2,iterations=2)
+	#temp_img = cv2.erode(binary_img,kernel,iterations=2)	
 	line_img = cv2.dilate(temp_img,kernel,iterations=5)
 	
 	(contours, _) = cv2.findContours(line_img.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -93,75 +64,132 @@ def process_line(thresh,output):
 	return output	
 
 #processing par by par boxing
-def process_par(thresh,output):	
+def process_par(binary_img, output, distinguish_img:bool=True, blur_count:int=1):	
 	# assign a rectangle kernel size
-	kernel = np.ones((5,5), 'uint8')	
-	par_img = cv2.dilate(thresh,kernel,iterations=3)
-	
+
+	for _ in range(blur_count):
+		binary_img = cv2.medianBlur(binary_img, 3)
+
+	kernel = np.ones((5,5), 'uint8')
+	par_img = cv2.dilate(binary_img, kernel, iterations=3)
+
+	par_shape = par_img.shape
+
+	gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+	shape = gray.shape
+		
 	(contours, _) = cv2.findContours(par_img.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	
 	for cnt in contours:
 		x,y,w,h = cv2.boundingRect(cnt)
-		cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),1)
+		if distinguish_img:
+			if x+w > shape[1] or y+h > shape[0]:  #1505,1109
+				print(x+w, ",", y+h)
+			else:
+				# another way
+				# mask = np.zeros(gray.shape[:2], np.uint8)
+				# mask[y: y+h, x: x+w] = 255
+				# hist = cv2.calcHist([gray], [0], mask, [256], [0, 256])
+				
+				hist = cv2.calcHist([gray[y: y+h, x: x+w]], [0], None, [256], [0, 256])
+				count = 0
+				for item in hist[30:240]:
+					count += item[0]
+				for item in hist[:30]:
+					count -= item[0]
+				for item in hist[-30:]:
+					count -= item[0]
 
+				count /= h*w
+
+				if count > 0.1:
+					cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+					cv2.putText(output, str(count), (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), 1)
+				elif count != -1:
+					cv2.rectangle(output,(x,y),(x+w,y+h),(255,0,0),2)
+					cv2.putText(output, str(count), (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 0, 0), 1)
+
+		else:
+			cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),1)
 	return output	
 
 #processing margin with paragraph boxing
-def process_margin(thresh,output):	
+def process_margin(binary_img,output):	
 	# assign a rectangle kernel size
 	kernel = np.ones((20,5), 'uint8')	
-	margin_img = cv2.dilate(thresh,kernel,iterations=5)
+	margin_img = cv2.dilate(binary_img,kernel,iterations=5)
 	
 	(contours, _) = cv2.findContours(margin_img.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	
+
 	for cnt in contours:
 		x,y,w,h = cv2.boundingRect(cnt)
 		cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),1)
 
 	return output
 
+#"journal1.jpg"
+def processImg(file_path):
+	if not os.path.exists(file_path):
+		raise FileNotFoundError("file not exist!")
 
-# processing and writing the output
-output1_letter = process_letter(th1,output1_letter)
-output1_word = process_word(th1,output1_word)
-output1_line = process_line(th1,output1_line)
-# special case for the 5th output because margin with paragraph is just the 4th output with margin
-cv2.imwrite("output/letter/output1_letter.jpg", output1_letter)	
-cv2.imwrite("output/word/output1_word.jpg", output1_word)
-cv2.imwrite("output/line/output1_line.jpg", output1_line)
-output1_par = process_par(th1,output1_par)
-cv2.imwrite("output/par/output1_par.jpg", output1_par)
-output1_margin = process_margin(th1,output1_par)
-cv2.imwrite("output/margin/output1_margin.jpg", output1_par)
+	start_time = timeit.default_timer()
 
-output2_letter = process_letter(th2,output2_letter)
-output2_word = process_word(th2,output2_word)
-output2_line = process_line(th2,output2_line)
+	# loading images
+	image = cv2.imread(file_path)
 
-cv2.imwrite("output/letter/output2_letter.jpg", output2_letter)	
-cv2.imwrite("output/word/output2_word.jpg", output2_word)
-cv2.imwrite("output/line/output2_line.jpg", output2_line)
-output2_par = process_par(th2,output2_par)
-cv2.imwrite("output/par/output2_par.jpg", output2_par)
-output2_margin = process_margin(th2,output2_par)
-cv2.imwrite("output/margin/output2_margin.jpg", output2_par)
+	# hardcoded assigning of output images for the 3 input images
+	output_letter = image.copy()
+	output_word = image.copy()
+	output_line = image.copy()
+	output_par = image.copy()
+	output_margin = image.copy()
 
-output3_letter = process_letter(th3,output3_letter)
-output3_word = process_word(th3,output3_word)
-output3_line = process_line(th3,output3_line)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite("output/letter/output3_letter.jpg", output3_letter)	
-cv2.imwrite("output/word/output3_word.jpg", output3_word)
-cv2.imwrite("output/line/output3_line.jpg", output3_line)
-output3_par = process_par(th3,output3_par)
-cv2.imwrite("output/par/output3_par.jpg", output3_par)
-output3_margin = process_margin(th3,output3_par)
-cv2.imwrite("output/margin/output3_margin.jpg", output3_par)
+	# clean the image using otsu method with the inversed binarized image
+	ret,th = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-#cv2.imshow("output letter", output1_letter)
-#cv2.imshow("output word", output1_word)
-#cv2.imshow("output line", output1_line)
-#cv2.imshow("output par", output1_par)
-#cv2.imshow("output margin", output1_par)
+	file_path = os.path.basename(file_path)
+	file_path = file_path.split('.')[0]
+	letter_path = os.path.join('output_' + file_path, 'letter')
+	word_path = os.path.join('output_' + file_path, 'word')
+	line_path = os.path.join('output_' + file_path, 'line')
+	par_path = os.path.join('output_' + file_path, 'par')
+	margin_path = os.path.join('output_' + file_path, 'margin')
+	os.makedirs(letter_path)
+	os.makedirs(word_path)
+	os.makedirs(line_path)
+	os.makedirs(par_path)
+	os.makedirs(margin_path)
 
-cv2.waitKey(0)
+	# processing and writing the output
+	output_letter = process_letter(th,output_letter)
+	output_word = process_word(th,output_word)
+	output_line = process_line(th,output_line)
+	# special case for the 5th output because margin with paragraph is just the 4th output with margin
+	cv2.imwrite(letter_path + "/output_letter.jpg", output_letter)	
+	cv2.imwrite(word_path + "/output_word.jpg", output_word)
+	cv2.imwrite(line_path + "/output_line.jpg", output_line)
+
+	# 图像噪声较多时，blur_count可以多一点(3)；需要找出的图片以线条为主时，blur_count要少(0)
+	output_par = process_par(th, output_par, distinguish_img=True, blur_count=0) 
+	cv2.imwrite(par_path + "/output_par.jpg", output_par)
+	output_margin = process_margin(th,output_par)
+	cv2.imwrite(margin_path + "/output_margin.jpg", output_par)
+
+	end_time = timeit.default_timer()
+	print('Total time: ', end_time-start_time)
+
+
+if __name__ == '__main__':
+	
+	import os
+	os.chdir('E:\wxy-git-document-layout-analysis-master\document-layout-analysis')
+
+	processImg('2.jpg')
+
+
+	# 2.jpg  	    0.018629    855*530
+	# 1.jpg  	    0.014034    636*524
+	# news1.jpg     0.175249    2500*1909
+	# journal1.jpg  0.040054    1109*1505
